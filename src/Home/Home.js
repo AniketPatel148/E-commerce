e3d2d761d8e789dcb5bafb13d380951b0b3f9817
Aniket Patel <aniket.r.21102001@gmail.com>
2020-12-22 08:29:22 +0530

    create navbar

---
+++ src/Home/Home.js
@@ -0,0 +1,20 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return (
+        <div className='home'>
+            <div className="home__container">
+            <img
+                className='home__image'
+                src='https://m.media-amazon.com/images/G/31/AmazonVideo/2019/1102620_MLP_1440x675_apv189_V3._SY1200_FMJPG_.jpg' />
+
+            </div>
+            
+        </div>
+
+    )
+}
+
+export default Home
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBXUFab3hELa0ygGmBiwi5bvvwmzej3IwY",
+    authDomain: "clone-4c2d0.firebaseapp.com",
+    projectId: "clone-4c2d0",
+    storageBucket: "clone-4c2d0.appspot.com",
+    messagingSenderId: "587093670824",
+    appId: "1:587093670824:web:6ea4e910f642b076f0fb9d"
+  }

dac0b302b2a5dd768e2c0de4906bac86b0547895
Aniket Patel <aniket.r.21102001@gmail.com>
2020-12-23 17:25:21 +0530

    create checkout page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import './App.css';
-import Header from './Header/Header';
-import Home from './Home/Home';
+import "./App.css";
+import Header from "./Header/Header";
+import Home from "./Home/Home";
+
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout/Checkout";
 
 function App() {
-  return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<div className="app">
+				<Header />
+				<Switch>
+					<Route path="/checkout">
+						<Checkout />
+					</Route>
+					<Route path="/">
+						<Home />
+					</Route>
+				</Switch>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ src/Checkout/Checkout.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function CHeckout() {
+	return (
+		<div className="checkout">
+			<div className="checkout__left">
+				<img
+					className="checkout__advertisement"
+					src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8HZBBtsqzdEBPX3qnLzR4gizYD4EGKzX2Zg&usqp=CAU"
+					alt=""
+				/>
+			</div>
+		</div>
+	);
+}
+
+export default CHeckout;

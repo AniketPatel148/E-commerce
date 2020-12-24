b27855d152b8e2912e33ba8179ffdd4f129d8080
Aniket Patel <aniket.r.21102001@gmail.com>
2020-12-24 11:43:35 +0530

    chechout right create

--- src/Checkout/Checkout.js
+++ src/Checkout/Checkout.js
@@ -1,6 +1,8 @@
 import React from "react";
+import Subtotal from "../Subtotal/Subtotal";
+import "./Checkout.css";
 
-function CHeckout() {
+function Checkout() {
 	return (
 		<div className="checkout">
 			<div className="checkout__left">
@@ -9,9 +11,15 @@ function CHeckout() {
 					src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8HZBBtsqzdEBPX3qnLzR4gizYD4EGKzX2Zg&usqp=CAU"
 					alt=""
 				/>
+				<div>
+					<h2 className="checkout__title">Your Shopping Basket!</h2>
+				</div>
+			</div>
+			<div className="checkout__right">
+				<Subtotal />
 			</div>
 		</div>
 	);
 }
 
-export default CHeckout;
+export default Checkout;
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,57 +1,47 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
 
-import SearchIcon from '@material-ui/icons/Search';
-import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { Link } from "react-router-dom";
 
 function Header() {
-    return (
-        <div className='header'>
-            <img 
-              className='header__image'
-              src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' 
+	return (
+		<div className="header">
+			<Link to="/">
+				<img
+					className="header__image"
+					src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+				/>
+			</Link>
+			<div className="header__search">
+				<input className="header__searchInput" type="text" />
+				<SearchIcon className="header__searchIcon" />
+			</div>
 
-              />
-            <div className='header__search'>
-
-                <input className='header__searchInput' 
-                    type='text' />
-                <SearchIcon className="header__searchIcon"/>
-            </div>
-
-            <div className="header__nav">
-                <div className="header__option">
-                    <span className="header__optionsTop">
-                        Hello buddy!
-                    </span>
-                    <span className="header__optionBottom">
-                        Sign In
-                    </span>
-                </div>
-                <div className="header__option">
-                    <span className="header__optionsTop">
-                        Hello buddy!
-                    </span>
-                    <span className="header__optionBottom">
-                        Sign In
-                    </span>
-                </div>
-                <div className="header__option">
-                    <span className="header__optionsTop">
-                        Hello buddy!
-                    </span>
-                    <span className="header__optionBottom">
-                        Sign In
-                    </span>
-                </div>
-                <div className="header__optionBasket">
-                    <ShoppingBasketIcon />
-                    <span className="header__optionBottom header__basketCount">0</span>
-                </div>
-            </div>
-        </div>
-    )
+			<div className="header__nav">
+				<div className="header__option">
+					<span className="header__optionsTop">Hello buddy!</span>
+					<span className="header__optionBottom">Sign In</span>
+				</div>
+				<div className="header__option">
+					<span className="header__optionsTop">Hello buddy!</span>
+					<span className="header__optionBottom">Sign In</span>
+				</div>
+				<div className="header__option">
+					<span className="header__optionsTop">Hello buddy!</span>
+					<span className="header__optionBottom">Sign In</span>
+				</div>
+				<Link to="/checkout">
+					<div className="header__optionBasket">
+						<ShoppingBasketIcon />
+						<span className="header__optionBottom header__basketCount">0</span>
+					</div>
+				</Link>
+			</div>
+		</div>
+	);
 }
 
-export default Header
+export default Header;
